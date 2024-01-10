5b47a24b0a2cdf2322bf6779bdbd8ae21f907d5d
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-01-09 19:15:28 -0500

    Add tour booking (but in Madelyn's account, we will change it later)

--- scripts/bookatour.js
+++ scripts/bookatour.js
@@ -1,17 +1,28 @@
+import db from "./firebase.js";
+
 const form = document.getElementById("form");
 
 const nameInput = document.getElementById("name-input");
 const dateInput = document.getElementById("date-input");
 const timeInput = document.getElementById("time-input");
 
-const submitButton = document.getElementById("submit-button");
-
 const book = function(name, date, time) {
-    
+    db.collection("bookings").doc().set({
+        name: name,
+        date: date,
+        time: time,
+    })
+    .then(() => {
+        console.log("Document successfully written!");
+    })
+    .catch((error) => {
+        console.error("Error writing document: ", error);
+    });
 }
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     book(nameInput.value, dateInput.value, timeInput.value);
+    form.reset();
 });
--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,10 @@
-document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="/World-Exploration-Center/styles/header.css" />`
+document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="/styles/header.css" />`;
 
 document.body.innerHTML = `
 <div id="header">
-    <a id="logo-link" href="/World-Exploration-Center/"><h1 id="logo">WEC</h1></a>
+    <a id="logo-link" href=""><h1 id="logo">WEC</h1></a>
 
-    <a class="header-link" href="/World-Exploration-Center/book-a-tour">Book a tour</a>
+    <a class="header-link" href="bookatour.html">Book a tour</a>
 </div>
 
 ${document.body.innerHTML}
